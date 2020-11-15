fe590284c431321ed7db1fd8d0f01cf7c4f48bff
parvanehpasha <parvaneh.moradipasha@gmail.com>
2020-11-15 13:51:11 +0330

    create view set

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
             'User Http get push create and delete',
@@ -11,3 +16,32 @@ class HelloApiView(APIView):
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
 
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+
+
+class ViewHelloSet(viewsets.ViewSet):
+
+    def list(self, request):
+        a_viewset = [
+            'added list create update and delete'
+        ]
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
